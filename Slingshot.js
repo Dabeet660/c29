e92c9e04ad77b6138722f314e106e2ec178660d3
DabeetDas <46596266+DabeetDas@users.noreply.github.com>
2020-08-11 16:44:38 +0530

    c29 commit

--- Slingshot.js
+++ Slingshot.js
@@ -6,7 +6,10 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
-        this.pointB = pointB
+        this.pointB = pointB;
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png"); // Band 
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
@@ -15,13 +18,35 @@ class SlingShot{
         this.sling.bodyA = null;
     }
 
-    display(){
+    
+   display(){
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push();
+            stroke(91, 31, 3);
+            
+            if(pointA.x < 220){
+                strokeWeight(7);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y); // line 1 (right side) 
+                line(pointA.x-20, pointA.y, pointB.x+30,pointB.y-3); // line 2 (left side)
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30);
+            } else { 
+                strokeWeight(3);
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y); // line 1 (right side) 
+                line(pointA.x+25, pointA.y, pointB.x+30,pointB.y-3); // line 2 (left side)
+                image(this.sling3,pointA.x+25,pointA.y-10,15,30);
+            }
+            pop();
         }
+
+        var sling1Img = this.sling1;
+        var sling2Img = this.sling2;
+        
+        image(sling1Img,200,20);
+        image(sling2Img,170,20);
+
     }
     
 }
